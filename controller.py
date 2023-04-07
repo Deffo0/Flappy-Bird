cb61247d0979d5c14cefe2b65ef886afe0702cd1
Deffo0 <ahmeddeffo777@gmail.com>
2023-04-07 13:52:06 +0200

    V2.0
    - add some animation to the bird

--- controller.py
+++ controller.py
@@ -9,14 +9,15 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 font = pygame.font.Font(None, 32)
 bg_img = pygame.image.load("Resources/BG.png")
-bird_img = pygame.image.load("Resources/bird.png")
+bird_images = [pygame.image.load(f"Resources/bird/skeleton-animation_{i}.png") for i in range(11)]
 pipe_img = pygame.image.load("Resources/pipe.png")
 pipe_reverse_img = pygame.image.load("Resources/pipe-reverse.png")
 game_over_img = pygame.image.load("Resources/game-over.png")
 game_over_sound = pygame.mixer.Sound("Resources/game_over.wav")
 
-x = 100
-y = HEIGHT // 2
+animation_state = 0
+bird_x = 100
+bird_y = HEIGHT // 2
 pipe_x = WIDTH
 pipe_y = 500
 pipe_reverse_y = 0
@@ -27,7 +28,7 @@ pipe_speed = 10
 
 
 def update_pipe():
-    global pipe_speed, pipe_type, pipe_x, WIDTH
+    global pipe_speed, pipe_type, pipe_x
     pipe_x -= pipe_speed
     if pipe_x < -100:
         pipe_x = WIDTH
@@ -35,17 +36,17 @@ def update_pipe():
 
 
 def update_bird():
-    global x, y, WIDTH
-    x += 10
-    y += 5
-    if x > WIDTH:
-        x = -50
+    global bird_x, bird_y
+    bird_x += 10
+    bird_y += 5
+    if bird_x > WIDTH:
+        bird_x = -50
 
 
 def draw():
-    global x, y, pipe_x, pipe_y, pipe_reverse_y, pipe_type
+    global bird_x, bird_y, pipe_x, pipe_y, pipe_reverse_y, pipe_type, animation_state
     win.blit(bg_img, (0, 0))
-    win.blit(bird_img, (x, y))
+    win.blit(bird_images[animation_state], (bird_x, bird_y))
     if pipe_type:
         win.blit(pipe_reverse_img, (pipe_x, pipe_reverse_y))
     else:
@@ -54,10 +55,10 @@ def draw():
     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
     score_rect = score_text.get_rect(topleft=(10, 10))
     win.blit(score_text, score_rect)
+    animation_state = (animation_state + 1) % 11
 
 
 def game_over():
-    global WIDTH, HEIGHT
     game_over_sound.play()
     win.blit(game_over_img, ((WIDTH / 2) - 215, (HEIGHT / 2) - 206))
     pygame.display.flip()
@@ -78,11 +79,11 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        y -= 50
+        bird_y -= 50
     else:
-        y += bird_speed
+        bird_y += bird_speed
 
-    bird_rect = bird_img.get_rect(topleft=(x, y))
+    bird_rect = bird_images[animation_state].get_rect(topleft=(bird_x, bird_y))
     if pipe_type:
         pipe_rect = pipe_reverse_img.get_rect(topleft=(pipe_x, pipe_reverse_y))
     else:
@@ -93,5 +94,5 @@ while True:
         pygame.quit()
         sys.exit()
 
-    if pipe_x < x < pipe_x + pipe_rect.width:
+    if pipe_x < bird_x < pipe_x + pipe_rect.width:
         score += 1
